f3d326c12d34a8f8a9255cccc4b9a5cf223c3bd9
Anshitachoubey07 <92245615+Anshitachoubey07@users.noreply.github.com>
2023-03-19 13:36:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function toggle(){
+    document.getElementById("hidden").style.display = "block";
+}
+var change = document.getElementById("hamburger");
+
+change.addEventListener(
+  "click",
+  toggle
+);
+
